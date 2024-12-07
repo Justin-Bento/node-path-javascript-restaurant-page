7c73e2d865d54e7c6014a9c0eaa4872e1eae668d
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-07 13:02:30 -0500

    Moved menu informtion to it's dedicated page

---
+++ src/api/menu.js
@@ -0,0 +1,22 @@
+export const specials = [
+  {
+    name: "Sanafé Chicken Plate",
+    description: "Juicy grilled or fried chicken in signature sanafé sauce, served with rice and veggies.",
+    price: "$18",
+  },
+  {
+    name: "The 'That’s Too Many Toppings' Burger",
+    description: "Beef patty topped with sanafé sauce, bacon, cheddar, fried onion strings, and a fried egg.",
+    price: "$17",
+  },
+  {
+    name: "Huddy's Cajun Alfredo",
+    description: "Fettuccine in creamy Cajun Alfredo sauce with grilled chicken or shrimp.",
+    price: "$20",
+  },
+  {
+    name: "The 'Actually, We’re Out' Special",
+    description: "Pan-seared salmon (or chicken), served with mashed potatoes and green beans.",
+    price: "$22",
+  },
+];
