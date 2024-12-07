dfd16a56b520f90aad869632457cf4a3a45b6460
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-06 22:27:04 -0500

    Looped url_locations list to display different data.

--- src/components/header_content.js
+++ src/components/header_content.js
@@ -14,6 +14,7 @@ export const header_content = () => {
   content.appendChild(companyLogo);
 
   // Section for creating and handling navigation links
+  const url_locations = ["Home", "Menu", "Location", "Our Story", "Contact"];
   const websiteNavigation = document.createElement("nav");
   websiteNavigation.setAttribute("class", "website-navigation"); // Added a class for styling
   content.appendChild(websiteNavigation);
@@ -22,12 +23,14 @@ export const header_content = () => {
   websiteNavigation_list.setAttribute("class", "navigation-list"); // Added a class for styling
   websiteNavigation.appendChild(websiteNavigation_list);
 
-  const websiteNavigation_list_item = document.createElement("li");
-  websiteNavigation_list_item.setAttribute("class", "navigation-list-item"); // Added a class for styling
-  websiteNavigation_list.appendChild(websiteNavigation_list_item);
+  url_locations.forEach((location) => {
+    const websiteNavigation_list_item = document.createElement("li");
+    websiteNavigation_list_item.setAttribute("class", "navigation-list-item"); // Added a class for styling
+    websiteNavigation_list.appendChild(websiteNavigation_list_item);
 
-  const websiteNavigation_list_item_action = document.createElement("button");
-  websiteNavigation_list_item_action.setAttribute("class", "navigation-button"); // Added a class for styling
-  websiteNavigation_list_item_action.textContent = "Home";
-  websiteNavigation_list_item.appendChild(websiteNavigation_list_item_action);
+    const websiteNavigation_list_item_action = document.createElement("button");
+    websiteNavigation_list_item_action.setAttribute("class", "navigation-button"); // Added a class for styling
+    websiteNavigation_list_item_action.textContent = location;
+    websiteNavigation_list_item.appendChild(websiteNavigation_list_item_action);
+  });
 };
