71a7d5c63f1a6ecd2d4a1b483564110b7b428a04
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-07 13:36:40 -0500

    added a public path for the output

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "/node-path-javascript-restaurant-page/",
   },
   devtool: "eval-source-map",
   devServer: {
