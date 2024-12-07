672100e275ec69b68e88375872b2ea50a6065928
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-07 12:41:00 -0500

    Added a banner of the bistro huddy crew

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,17 @@
+import CompanyLogo from "../assets/resource/bistrohuddy_crew.png";
+
 export const about = () => {
   const content = document.querySelector("#content");
   // Clear existing content to prevent duplicates
   content.innerHTML = "";
+
+  // Create and append the company logo
+  const companyLogo = document.createElement("img");
+  companyLogo.setAttribute("src", CompanyLogo);
+  companyLogo.setAttribute("alt", "Bistro Huddy: Company Logo");
+  companyLogo.setAttribute("class", "company-crew");
+  content.appendChild(companyLogo);
+
   // Section for creating elements
   const headline = document.createElement("h1");
   const supporting = document.createElement("p");
