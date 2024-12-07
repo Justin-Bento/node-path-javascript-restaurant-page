78d5d061a849b497abfdf53a9159df9ef1bc22b3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-07 13:12:49 -0500

    Updated content

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,7 +19,15 @@ export const home = () => {
   // Section for creating and appending elements
   wrapper.appendChild(Text("h1", "Welcome To Bistro Huddy"));
   wrapper.appendChild(
-    Text("p", "Bistro Huddy is a fictional restaurant and set created by Drew Talbert and his wife, Andrea Kelley.")
+    Text(
+      "p",
+      "Bistro Huddy is a workplace comedy solo performance web series created and written by Drew Talbert and Andrea Kelley."
+    )
+  );
+  wrapper.appendChild(
+    Text(
+      "p",
+      "Set in Los Angeles in the titular restaurant, it revolves around the daily lives of the Bistro Huddy staff and their guests."
+    )
   );
 };
-x`x`;
