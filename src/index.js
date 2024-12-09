ec5b0b69c4f84311e44d258dd716be5339ef5024
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-09 11:30:18 -0500

    reset the document

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 import { Header } from "./components/header.js";
 import { Footer } from "./components/footer.js";
+import { Home } from "./pages/home.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   Header();
+  Home();
   Footer();
 });
