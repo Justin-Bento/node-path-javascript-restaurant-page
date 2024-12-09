e396eafa570a0a0d7fbce3b277450ab8414f6705
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-09 14:00:32 -0500

    Appended two paragraphs to the main home.

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,4 +22,16 @@ export const Home = () => {
   const heading1 = document.createElement("h1");
   heading1.textContent = "Bistro Huddy";
   wrapper.appendChild(heading1);
+
+  // Create a heading element and append it to the wrapper
+  const paragraph = document.createElement("p");
+  paragraph.textContent =
+    "Bistro Huddy is a workplace comedy solo performance web series created and written by Drew Talbert and Andrea Kelley.";
+  wrapper.appendChild(paragraph);
+
+  // Create a heading element and append it to the wrapper
+  const paragraph2 = document.createElement("p");
+  paragraph2.textContent =
+    "Set in Los Angeles in the titular restaurant, it revolves around the daily lives of the Bistro Huddy staff and their guests.";
+  wrapper.appendChild(paragraph2);
 };
