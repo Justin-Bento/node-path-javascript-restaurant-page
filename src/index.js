c12b167ac11cd0357eab6fac2a83d042e8cc24ff
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-09 11:05:00 -0500

    Transferred assets to main page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import { Header } from "./components/header.js";
+import { Footer } from "./components/footer.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  Header();
+  Footer();
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      // CSS Loader
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      // HTML Loader
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      // Fonts Loader
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource", // Correct type for static assets
+        generator: {
+          filename: "resource/fonts/[name][ext]", // Output fonts to "resource/fonts/"
+        },
+      },
+      // Images Loader
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource", // Correct type for static assets
+        generator: {
+          filename: "resource/images/[name][ext]", // Output images to "resource/images/"
+        },
+      },
+    ],
+  },
+};
