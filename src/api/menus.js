5c33de059f08972c48eaf3b40f36ff1efa4623df
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-12-19 11:25:54 -0400

    Created the api for the menu

---
+++ src/api/menus.js
@@ -0,0 +1,46 @@
+export const food = {
+  smallBites: [
+    {
+      id: 1,
+      name: "Ahi Tartare",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$12",
+    },
+    {
+      id: 2,
+      name: "Calamari",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$14",
+    },
+    {
+      id: 3,
+      name: "Fried Goat Cheese ",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$18",
+    },
+    {
+      id: 4,
+      name: "Green Chile Queso or Quesdilla",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$18",
+    },
+    {
+      id: 5,
+      name: "Spicy Fried Dates",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$10",
+    },
+    {
+      id: 6,
+      name: "Stuffed Squash Blossom",
+      description: "The quick brown fox jumps over the lazy dog.",
+      price: "$21",
+    },
+  ],
+  mainDishes: {},
+  desserts: {},
+  Sides: {},
+  kidsMenu: {},
+  CondimentsOrDressings: {},
+  drinks: {},
+};
